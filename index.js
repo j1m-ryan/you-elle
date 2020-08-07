cad5f82ddfcdc291a61c6b6623866a63a2e40849
J1M-RYAN <jimmih15@gmail.com>
2020-08-07 15:54:58 +0100

    inquirer options

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 #!/usr/bin/env node
+const https = require("https");
+const { program, option } = require("commander");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
-console.log("Welcome to you-elle, the UL timetable cli");
-if (process.argv.length < 4) {
+https
+  .get("https://www.timetable.ul.ie/UA/CourseTimetable.aspx", (resp) => {
+    let data = "";
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+    resp.on("end", () => {
+      const dom = new JSDOM(data);
+      const options = dom.window.document.getElementsByTagName("option");
+      const courses = [];
+      for (let i = 7; i < options.length; i++) {
+        courses.push(options[i].textContent);
+      }
+      const questions = [
+        {
+          type: "list",
+          name: "year",
+          message: "Which year are you in?",
+          choices: [1, 2, 3, 4, 5],
+        },
+        {
+          type: "list",
+          name: "course",
+          message: "Which course are you in?",
+          choices: courses,
+        },
+      ];
+      inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+      });
+
+      /* for (let i = 7; i < options.length; i++) {
+        console.log(options[i].textContent);
+      } */
+    });
+  })
+  .on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+console.log(chalk.green("Welcome to you-elle, the UL timetable cli"));
+
+/* if (process.argv.length < 4) {
   console.log("Please specify a year and a course");
   console.log("Example usage: $ you-elle 1 LM121");
 } else if (process.argv.length == 4) {
@@ -10,7 +56,7 @@ if (process.argv.length < 4) {
   if (!isInteger(year) || year > 4 || year < 1) {
     console.log("Enter a valid year between 1 and 4");
     process.exit(1);
-  } else if (course.length < 5) {
+  } else if (course.length >= 5) {
     console.log("Enter a valid course");
     process.exit(1);
   }
@@ -22,3 +68,4 @@ if (process.argv.length < 4) {
 function isInteger(value) {
   return /^\d+$/.test(value);
 }
+ */
